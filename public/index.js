3f499b2598b8c0c5c95459c8dd52267a0404ddbf
Parul-Saini <sainiparul700@gmail.com>
2022-09-01 17:49:39 +0530

    change the file folder

---
+++ public/index.js
@@ -0,0 +1,56 @@
+// e1ec161d4452493abb9b8bbfee104080
+//https://newsapi.org/v2/top-headlines?country=in&apiKey=e1ec161d4452493abb9b8bbfee104080
+
+
+
+// Initialize the news api parameters
+const apikey="e1ec161d4452493abb9b8bbfee104080";
+
+// Grab the news container
+let newsaccordion =document.getElementById("newsaccordion");
+
+// Create an ajax get request
+const xhr= new XMLHttpRequest();
+
+xhr.open('GET',`https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}`,true);
+
+xhr.onload=function(){
+    if(this.status=== 200){
+       let json =JSON.parse(this.responseText);
+       let articles = json.articles;
+       console.log(articles);
+       
+       let newshtml="";
+
+       articles.forEach((element,index) => {
+        let news = `<div class="card" style="border:none">
+                <div class="card-header my-2" id="heading${index}"  style="background-color: #0f1418;border: solid 8px #007bff;>
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                        aria-expanded="true" aria-controls="collapse${index}">
+                      <b>BREAKING NEWS:${index+1} </b> ${element["title"]}
+                    </button>
+                </h2>
+                </div>
+
+                <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsaccordion">
+                    <div class="card-body">
+                    ${element["content"]}
+                    ..<a href="${element['url']} "target="_blank">Read More</a>
+                    </div>
+                </div>
+            </div>`
+        newshtml+= news;
+       });
+       newsaccordion.innerHTML=newshtml; 
+
+    }
+    else{
+    console.log("some error occur");
+    }
+}
+
+xhr.send();
+
+// What to do when response is ready
+
